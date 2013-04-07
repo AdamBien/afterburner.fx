8f40b53b5a8476fb8e909167406894c96bb7f8dd
Adam Bien <abien@adam-bien.com>
2013-04-07 05:58:00 +0200

    Cleanup

--- src/main/java/com/airhacks/afterburner/injection/InjectionProvider.java
+++ src/main/java/com/airhacks/afterburner/injection/InjectionProvider.java
@@ -19,14 +19,11 @@ package com.airhacks.afterburner.injection;
  * limitations under the License.
  * #L%
  */
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
