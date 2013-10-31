c4e2db0868b428edf65f49d30d57b7accb7deef7
Adam Bien <abien@adam-bien.com>
2013-10-31 06:13:03 +0100

    Apache header added

--- src/test/java/com/airhacks/afterburner/injection/Presenter.java
+++ src/test/java/com/airhacks/afterburner/injection/Presenter.java
@@ -1,6 +1,11 @@
+package com.airhacks.afterburner.injection;
+
 /*
- * Copyright 2013 Adam Bien.
- *
+ * #%L
+ * afterburner.fx
+ * %%
+ * Copyright (C) 2013 Adam Bien
+ * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,9 +17,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * #L%
  */
-package com.airhacks.afterburner.injection;
-
 /**
  *
  * @author adam-bien.com
