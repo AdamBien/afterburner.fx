5dade006a4f1b16a368e837dcbae9c1793780328
Adam Bien <abien@adam-bien.com>
2013-12-16 19:23:54 +0100

    Additional integration tests implemented

--- src/test/java/com/airhacks/afterburner/views/TopgunViewTest.java
+++ src/test/java/com/airhacks/afterburner/views/TopgunViewTest.java
@@ -12,6 +12,20 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *//*
+ * Copyright 2013 Adam Bien.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.airhacks.afterburner.views;
 
@@ -34,12 +48,14 @@ package com.airhacks.afterburner.views;
  * limitations under the License.
  * #L%
  */
-
 import com.airhacks.afterburner.topgun.TopgunPresenter;
 import com.airhacks.afterburner.topgun.TopgunView;
+import javafx.scene.Parent;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.Assert;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -48,14 +64,40 @@ import org.junit.Test;
  */
 public class TopgunViewTest {
 
+    private TopgunView view;
+    private TopgunPresenter presenter;
+
+    @Before
+    public void initialize() {
+        this.view = new TopgunView();
+        this.presenter = (TopgunPresenter) view.getPresenter();
+    }
+
     @Test
-    public void loadView() {
-        TopgunView view = new TopgunView();
-        TopgunPresenter presenter = (TopgunPresenter) view.getPresenter();
+    public void loadViewWithConfiguration() {
         Assert.assertNotNull(presenter);
         String host = presenter.getHost();
         Assert.assertNotNull(host);
         final String expected = "tower"; //from configuration.properties
         assertThat(host, is(expected));
     }
+
+    @Test
+    public void getView() {
+        Parent parent = this.view.getView();
+        assertNotNull(parent);
+    }
+
+    @Test
+    public void getPresenter() {
+        Object object = this.view.getPresenter();
+        assertNotNull(object);
+    }
+
+    @Test
+    public void getPresengetViewWithoutRootContainerter() {
+        Object object = this.view.getViewWithoutRootContainer();
+        assertNotNull(object);
+    }
+
 }
