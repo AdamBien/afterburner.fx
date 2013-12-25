9e5d4da944c381c545e62b49c586f6416c95c957
Adam Bien <abien@adam-bien.com>
2013-12-25 05:49:01 +0100

    Broken injection of services in superclasses reproduced

--- src/test/java/com/airhacks/afterburner/lifecycle/ModelLifecycleTest.java
+++ src/test/java/com/airhacks/afterburner/lifecycle/ModelLifecycleTest.java
@@ -24,9 +24,9 @@ package com.airhacks.afterburner.lifecycle;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,10 +34,10 @@ package com.airhacks.afterburner.lifecycle;
  * limitations under the License.
  * #L%
  */
-
 import com.airhacks.afterburner.injection.InjectionProvider;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.After;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
 import org.junit.Test;
@@ -62,6 +62,15 @@ public class ModelLifecycleTest {
         assertThat(initializationCount, is(1));
         int destructionCount = this.cut.getDestructionCount();
         assertThat(destructionCount, is(0));
+        InjectionProvider.forgetAll();
+        destructionCount = this.cut.getDestructionCount();
+        assertThat(destructionCount, is(1));
+    }
+
+    @Test
+    public void serviceInjectedInAbstractModel() {
+        ConcreteService injectedService = this.cut.getServiceInjectedInSuperclass();
+        assertNotNull(injectedService);
     }
 
     @After
