090df87ba41c070202c3771c14ba81ae126fb724
Adam Bien <abien@adam-bien.com>
2013-12-25 05:30:41 +0100

    Test for abstract lifecycle methods implemented

---
+++ src/test/java/com/airhacks/afterburner/lifecycle/LifecyclePresenter.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2013 Adam Bien.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.airhacks.afterburner.lifecycle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javax.inject.Inject;
+
+/**
+ * FXML Controller class
+ *
+ * @author adam-bien.com
+ */
+public class LifecyclePresenter implements Initializable {
+
+    @Inject
+    ConcreteModel concreteModel;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/test/java/com/airhacks/afterburner/lifecycle/ModelLifecycleTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2013 Adam Bien.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.airhacks.afterburner.lifecycle;
+
+import com.airhacks.afterburner.injection.InjectionProvider;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author adam-bien.com
+ */
+public class ModelLifecycleTest {
+
+    private ConcreteModel cut;
+
+    @Before
+    public void initialize() {
+        LifecyclePresenter presenter = (LifecyclePresenter) new LifecycleView().getPresenter();
+        this.cut = presenter.concreteModel;
+    }
+
+    @Test
+    public void abstractPostConstructCalled() {
+        int initializationCount = this.cut.getInitializationCount();
+        assertThat(initializationCount, is(1));
+        int destructionCount = this.cut.getDestructionCount();
+        assertThat(destructionCount, is(0));
+    }
+
+    @After
+    public void cleanUp() {
+        InjectionProvider.forgetAll();
+    }
+}
