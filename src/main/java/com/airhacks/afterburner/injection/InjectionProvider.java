6722ee34507e5efdfca50ef4010639049e0d7bc9
Adam Bien <abien@adam-bien.com>
2014-01-15 09:11:04 +0100

    putIfAbsent removed for jdk7 compatibility

--- src/main/java/com/airhacks/afterburner/injection/InjectionProvider.java
+++ src/main/java/com/airhacks/afterburner/injection/InjectionProvider.java
@@ -9,9 +9,9 @@ package com.airhacks.afterburner.injection;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -75,7 +75,9 @@ public class InjectionProvider {
         if (product == null) {
             try {
                 product = injectAndInitialize(clazz.newInstance());
-                modelsAndServices.putIfAbsent(clazz, product);
+                if (!modelsAndServices.containsKey(clazz)) {
+                    modelsAndServices.put(clazz, product);
+                }
             } catch (InstantiationException | IllegalAccessException ex) {
                 throw new IllegalStateException("Cannot instantiate view: " + clazz, ex);
             }
