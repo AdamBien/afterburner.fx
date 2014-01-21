2a9fb80ddb66f81310d547aab79b7b0eea993a9c
Adam Bien <abien@adam-bien.com>
2014-01-21 10:52:30 +0100

    FXML is loaded asynchronously

--- src/main/java/com/airhacks/afterburner/views/FXMLView.java
+++ src/main/java/com/airhacks/afterburner/views/FXMLView.java
@@ -9,9 +9,9 @@ package com.airhacks.afterburner.views;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,6 +25,11 @@ import java.net.URL;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 import static java.util.ResourceBundle.getBundle;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -37,22 +42,30 @@ import javafx.util.Callback;
 public abstract class FXMLView {
 
     public final static String DEFAULT_ENDING = "view";
-    protected FXMLLoader loader;
+    protected Future<FXMLLoader> lazyLoader;
+    private static final ExecutorService THREAD_POOL = Executors.newCachedThreadPool();
 
     public FXMLView() {
         this.init(getClass(), getFXMLName());
     }
 
-    private void init(Class clazz, String conventionalName) {
+    private void init(Class clazz, final String conventionalName) {
         final URL resource = clazz.getResource(conventionalName);
         String bundleName = getBundleName();
-        ResourceBundle bundle = getResourceBundle(bundleName);
-        loadAsynchronously(resource, bundle, conventionalName);
+        final ResourceBundle bundle = getResourceBundle(bundleName);
+        Callable<FXMLLoader> initialization = new Callable<FXMLLoader>() {
+
+            @Override
+            public FXMLLoader call() throws Exception {
+                return loadAsynchronously(resource, bundle, conventionalName);
+            }
+        };
+        this.lazyLoader = (Future<FXMLLoader>) THREAD_POOL.submit(initialization);
     }
 
-    void loadAsynchronously(final URL resource, ResourceBundle bundle, String conventionalName) throws IllegalStateException {
-        this.loader = new FXMLLoader(resource, bundle);
-        this.loader.setControllerFactory(new Callback<Class<?>, Object>() {
+    FXMLLoader loadAsynchronously(final URL resource, ResourceBundle bundle, String conventionalName) throws IllegalStateException {
+        FXMLLoader loader = new FXMLLoader(resource, bundle);
+        loader.setControllerFactory(new Callback<Class<?>, Object>() {
             @Override
             public Object call(Class<?> p) {
                 return InjectionProvider.instantiatePresenter(p);
@@ -63,10 +76,11 @@ public abstract class FXMLView {
         } catch (IOException ex) {
             throw new IllegalStateException("Cannot load " + conventionalName, ex);
         }
+        return loader;
     }
 
     public Parent getView() {
-        Parent parent = this.loader.getRoot();
+        Parent parent = getLoader().getRoot();
         addCSSIfAvailable(parent);
         return parent;
     }
@@ -100,8 +114,7 @@ public abstract class FXMLView {
     }
 
     public Object getPresenter() {
-        Object controller = this.loader.getController();
-        return controller;
+        return this.getLoader().getController();
     }
 
     String getConventionalName(String ending) {
@@ -135,6 +148,14 @@ public abstract class FXMLView {
             return getBundle(name);
         } catch (MissingResourceException ex) {
             return null;
+        }
+    }
+
+    FXMLLoader getLoader() {
+        try {
+            return this.lazyLoader.get();
+        } catch (InterruptedException | ExecutionException ex) {
+            throw new IllegalStateException("Initialization problem", ex);
         }
     }
 }
