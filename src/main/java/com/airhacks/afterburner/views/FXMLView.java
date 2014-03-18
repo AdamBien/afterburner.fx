739307086e0445e9a9ae44532f7563469cdf8486
Adam Bien <abien@adam-bien.com>
2014-03-18 19:15:51 +0100

    Asynchronous loading of FXML is executed in the EDT first, then in Runnable

--- src/main/java/com/airhacks/afterburner/views/FXMLView.java
+++ src/main/java/com/airhacks/afterburner/views/FXMLView.java
@@ -9,9 +9,9 @@ package com.airhacks.afterburner.views;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
@@ -84,10 +85,10 @@ public abstract class FXMLView {
                 }
             }
         };
-
-        if (Platform.isFxApplicationThread()) {
+        try {
             Platform.runLater(runnable);
-        } else {
+        } catch (IllegalStateException ex) {
+            //can only happen in a headless unit test
             runnable.run();
         }
         return loader;
