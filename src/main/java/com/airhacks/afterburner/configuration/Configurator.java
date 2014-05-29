5edd716c7140b7be4c4fa469b0746a27fc9323f7
Adam Bien <abien@adam-bien.com>
2014-05-29 07:04:36 +0200

    SystemProperties supplier removed

--- src/main/java/com/airhacks/afterburner/configuration/Configurator.java
+++ src/main/java/com/airhacks/afterburner/configuration/Configurator.java
@@ -24,9 +24,9 @@ package com.airhacks.afterburner.configuration;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -47,15 +47,11 @@ public class Configurator {
 
     public final static String CONFIGURATION_FILE = "configuration.properties";
 
-    private Function<Object, Object> systemProperties;
+    private Properties systemProperties;
     private Function<Object, Object> customConfigurator;
 
     public Configurator() {
-        this.systemProperties = getSystemPropertiesSupplier();
-    }
-
-    public final Function<Object, Object> getSystemPropertiesSupplier() {
-        return k -> System.getProperty(k.toString());
+        this.systemProperties = System.getProperties();
     }
 
     public Configurator set(Function<Object, Object> custom) {
@@ -83,7 +79,7 @@ public class Configurator {
      * @return
      */
     public Object getProperty(Class clazz, Object key) {
-        Object value = this.systemProperties.apply(key);
+        Object value = this.systemProperties.get(key);
         if (value != null) {
             return value;
         }
@@ -93,7 +89,6 @@ public class Configurator {
                 return value;
             }
         }
-
         Properties clazzProperties = this.getProperties(clazz);
         if (clazzProperties != null) {
             value = clazzProperties.get(key);
--- src/test/java/com/airhacks/afterburner/configuration/ConfiguratorTest.java
+++ src/test/java/com/airhacks/afterburner/configuration/ConfiguratorTest.java
@@ -24,9 +24,9 @@ package com.airhacks.afterburner.configuration;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,7 +36,6 @@ package com.airhacks.afterburner.configuration;
  */
 import java.util.function.Function;
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import org.junit.Before;
@@ -53,14 +52,6 @@ public class ConfiguratorTest {
     @Before
     public void init() {
         this.cut = new Configurator();
-    }
-
-    @Test
-    public void getSystemPropertiesSupplier() {
-        Function<Object, Object> systemPropertiesSupplier = this.cut.getSystemPropertiesSupplier();
-        assertNotNull(systemPropertiesSupplier);
-        Object apply = systemPropertiesSupplier.apply("java.version");
-        assertNotNull(apply);
     }
 
     @Test
