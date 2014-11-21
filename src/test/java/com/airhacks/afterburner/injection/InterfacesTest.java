91ddccaafe0d3bd60d062179787e358e5aad1c0f
Adam Bien <abien@adam-bien.com>
2014-11-21 09:03:58 +0100

    Test fixed

--- src/test/java/com/airhacks/afterburner/injection/InterfacesTest.java
+++ src/test/java/com/airhacks/afterburner/injection/InterfacesTest.java
@@ -19,40 +19,37 @@ package com.airhacks.afterburner.injection;
  * limitations under the License.
  * #L%
  */
-
-import org.junit.Test;
-
-import com.airhacks.afterburner.injection.Injector;
 import com.airhacks.afterburner.injection.interfaces.InterfacePresenter;
 import com.airhacks.afterburner.injection.interfaces.InterfaceView;
 import com.airhacks.afterburner.injection.interfaces.Ping;
 import com.airhacks.afterburner.injection.interfaces.Pong;
 import com.airhacks.afterburner.injection.interfaces.SimplePing;
-
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import org.junit.Test;
 
 public class InterfacesTest {
-	@Test
-	public void canBuildAnInterfaceObjectDeclaredInServiceLoader() {
-		Object builtObject = Injector.getDefaultInstanceSupplier().apply(Ping.class);
-		
-		assertThat(builtObject, notNullValue());
-		assertThat(builtObject, instanceOf(SimplePing.class));
-	}
-	
-	@Test (expected=IllegalStateException.class)
-	public void failWhenNoInterfaceImplementationIsFound() {
-		Injector.getDefaultInstanceSupplier().apply(Pong.class);
-	}
 
-	@Test
-	public void canInjectAnInterfaceInAPresenter() {
-		InterfaceView iv = new InterfaceView();
-		InterfacePresenter p = (InterfacePresenter) iv.getPresenter();
-		
-		assertThat(p.getPing(), notNullValue());
-		assertThat(p.getPing(), instanceOf(SimplePing.class));
-	}
+    @Test
+    public void canBuildAnInterfaceObjectDeclaredInServiceLoader() {
+        Object builtObject = Injector.getDefaultInstanceSupplier().instanciate(Ping.class);
+
+        assertThat(builtObject, notNullValue());
+        assertThat(builtObject, instanceOf(SimplePing.class));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void failWhenNoInterfaceImplementationIsFound() {
+        Injector.getDefaultInstanceSupplier().instanciate(Pong.class);
+    }
+
+    @Test
+    public void canInjectAnInterfaceInAPresenter() {
+        InterfaceView iv = new InterfaceView();
+        InterfacePresenter p = (InterfacePresenter) iv.getPresenter();
+
+        assertThat(p.getPing(), notNullValue());
+        assertThat(p.getPing(), instanceOf(SimplePing.class));
+    }
 }
