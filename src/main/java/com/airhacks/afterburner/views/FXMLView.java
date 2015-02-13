f675cb994da25ae1b5ab0356ab2be557e8ccd719
Adam Bien <abien@adam-bien.com>
2015-02-13 08:38:39 +0100

    getConventionalName changed to protected

--- src/main/java/com/airhacks/afterburner/views/FXMLView.java
+++ src/main/java/com/airhacks/afterburner/views/FXMLView.java
@@ -9,9 +9,9 @@ package com.airhacks.afterburner.views;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -193,7 +193,7 @@ public abstract class FXMLView {
         });
     }
 
-    String getConventionalName(String ending) {
+    protected String getConventionalName(String ending) {
         return getConventionalName() + ending;
     }
 
