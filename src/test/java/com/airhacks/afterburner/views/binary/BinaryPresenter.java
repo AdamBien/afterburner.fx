b15421f564141206a93dd18b022a9759b0d8c767
Ben Oxley <ben.oxley@gmail.com>
2016-10-15 09:00:34 +0100

    Made changes to FXMLView to mirror work branch and added tests to check functionality

---
+++ src/test/java/com/airhacks/afterburner/views/binary/BinaryPresenter.java
@@ -0,0 +1,7 @@
+package com.airhacks.afterburner.views.binary;
+
+/**
+ * Created by ben on 15/10/2016.
+ */
+public class BinaryPresenter {
+}
---
+++ src/test/java/com/airhacks/afterburner/views/binary/BinaryView.java
@@ -0,0 +1,10 @@
+package com.airhacks.afterburner.views.binary;
+
+import com.airhacks.afterburner.views.FXMLView;
+
+/**
+ * Created by ben on 15/10/2016.
+ */
+public class BinaryView extends FXMLView {
+
+}
