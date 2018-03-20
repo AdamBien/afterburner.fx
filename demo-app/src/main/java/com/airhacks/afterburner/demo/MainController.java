6ac80fd808989d5375760ddeefb1ab1fb5b01777
Tobias Diez <tobiasdiez@gmx.de>
2018-03-20 17:36:47 +0100

    Add simple dialog

--- demo-app/src/main/java/com/airhacks/afterburner/demo/MainController.java
+++ demo-app/src/main/java/com/airhacks/afterburner/demo/MainController.java
@@ -1,7 +1,21 @@
 package com.airhacks.afterburner.demo;
 
+import javafx.fxml.FXML;
+
+import com.airhacks.afterburner.demo.control.CustomTextField;
+import com.airhacks.afterburner.demo.dialog.SimpleDialog;
+
 public class MainController {
+    @FXML private CustomTextField textField;
+
     public MainController() {
         System.out.println("Main controller constructed.");
+    }
+
+    @FXML
+    private void openDialog() {
+        SimpleDialog dialog = new SimpleDialog("Hello from main controller");
+        dialog.showAndWait()
+              .ifPresent(message -> textField.setText(message));
     }
 }
