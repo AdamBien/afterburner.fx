45bc1df45a6e8c6dd399d3fa791fa564a13d0e3b
Siedlerchr <siedlerkiller@gmail.com>
2022-09-26 20:53:29 +0200

    add uses declaration

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
+
 open module afterburner.fx{
 	 
 	 exports com.airhacks.afterburner.injection;
 	 exports com.airhacks.afterburner.configuration;
 	 exports com.airhacks.afterburner.views;
+	 
+     uses com.airhacks.afterburner.views.ResourceLocator;
+     uses com.airhacks.afterburner.injection.PresenterFactory;
 	 
 	 requires jakarta.annotation;
 	 requires org.slf4j;	
