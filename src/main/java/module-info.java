6087d5e70792d7e3a63a05455bc464b8d95cd2cf
Siedlerchr <siedlerkiller@gmail.com>
2022-09-26 21:30:23 +0200

    reformat

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,17 +1,16 @@
 
-open module afterburner.fx{
-	 
-	 exports com.airhacks.afterburner.injection;
-	 exports com.airhacks.afterburner.configuration;
-	 exports com.airhacks.afterburner.views;
-	 
-     uses com.airhacks.afterburner.views.ResourceLocator;
-     uses com.airhacks.afterburner.injection.PresenterFactory;
-	 
-	 requires jakarta.annotation;
-	 requires org.slf4j;	
-	 requires jakarta.inject;
-	 requires transitive javafx.fxml;
-	 requires transitive javafx.controls;
+open module afterburner.fx {
 
+	exports com.airhacks.afterburner.injection;
+	exports com.airhacks.afterburner.configuration;
+	exports com.airhacks.afterburner.views;
+
+	uses com.airhacks.afterburner.views.ResourceLocator;
+	uses com.airhacks.afterburner.injection.PresenterFactory;
+
+	requires jakarta.annotation;
+	requires org.slf4j;
+	requires jakarta.inject;
+	requires transitive javafx.fxml;
+	requires transitive javafx.controls;
 }
